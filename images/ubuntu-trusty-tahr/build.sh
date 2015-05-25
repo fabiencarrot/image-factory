87b61bfea161888e5c5335e5b1beb34c4cc5d420
Maury Aurelien <aurelienmaury@users.noreply.github.com>
2015-05-25 16:03:10 +0200

    tahr

--- images/ubuntu-trusty-tahr/build.sh
+++ images/ubuntu-trusty-tahr/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-BASENAME="ubuntu-trusty-tahr"
+BASENAME="ubuntu-14.04"
 TENANT_ID="772be1ffb32e42a28ac8e0205c0b0b90"
 BUILDMARK="$(date +%Y-%m-%d-%H%M)"
 IMG_NAME="$BASENAME-$BUILDMARK"
 TMP_IMG_NAME="$IMG_NAME-tmp"
 
-IMG=ubuntu-14.04-server-cloudimg-amd64.tar.gz
+IMG=ubuntu-14.04-server-cloudimg-amd64-disk1.img
 IMG_URL=http://cloud-images.ubuntu.com/releases/14.04/release/$IMG
 
 TMP_DIR=debian-jessie-guest
@@ -21,9 +21,9 @@ fi
 
 guestmount -a $IMG -i $TMP_DIR
 
-sed -i "s#name: debian#name: cloud#" $TMP_DIR/etc/cloud/cloud.cfg
-sed -i "s#gecos: Debian#gecos: Cloud user#" $TMP_DIR/etc/cloud/cloud.cfg
-sed -i "s#debian#cloud#" $TMP_DIR/etc/sudoers.d/debian-cloud-init
+sed -i "s#name: ubuntu#name: cloud#" $TMP_DIR/etc/cloud/cloud.cfg
+sed -i "s#gecos: Ubuntu#gecos: Cloud user#" $TMP_DIR/etc/cloud/cloud.cfg
+sed -i "s#ubuntu#cloud#" $TMP_DIR/etc/sudoers.d/debian-cloud-init
 sed -i "#ed25519#d" $TMP_DIR/etc/ssh/sshd_config
 sed -i "/gecos/a \ \ \ \ \ shell: \/bin\/bash" $TMP_DIR/etc/cloud/cloud.cfg
 
@@ -43,7 +43,7 @@ glance image-delete $TMP_IMG_NAME
 
 IMG_ID="$(glance image-list --owner $TENANT_ID --is-public False | grep $IMG_NAME | tr "|" " " | tr -s " " | cut -d " " -f2)"
 
-glance image-update --property cw_os=Debian --property cw_origin=Cloudwatt --property hw_rng_model=virtio --min-disk 10 --purge-props $IMG_ID
+glance image-update --property cw_os=Ubuntu --property cw_origin=Cloudwatt --property hw_rng_model=virtio --min-disk 10 --purge-props $IMG_ID
 
 FREE_FLOATING_IP="$(neutron floatingip-list | grep -v "+" | grep -v "id" | tr -d " " | grep -v -E "^\|.+\|.+\|.+\|.+\|$" | cut -d "|" -f 2)"
 
@@ -56,6 +56,5 @@ done
 echo "======= Cleaning too old images"
 
 glance image-list | grep $BASENAME | tr "|" " " | tr -s " " |cut -d " " -f 3 | sort -r | awk 'NR>5' | xargs glance image-delete
-
 
 glance image-show $IMG_ID
