7df0702bd52a074a0c091f25f0ed1fb75a7d5a8b
Maury Aurelien <aurelienmaury@users.noreply.github.com>
2015-05-26 15:22:53 +0200

    path mislead

--- test-tools/ala/run_tests.sh
+++ test-tools/ala/run_tests.sh
@@ -21,8 +21,17 @@ WIN_PASSWORD="ZC83nSQZHpVYtj"
 VOLUME_SIZE=1
 REBOOT=0
 
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+    # error; for some reason, the path is not accessible
+    # to the script (e.g. permissions re-evaled after suid)
+    exit 1  # fail
+fi
 
-source functions.sh
+
+
+source $MY_PATH/functions.sh
 
 if [[ ! ("$OS_TENANT_NAME" && "$OS_USERNAME" && "$OS_PASSWORD" && "$OS_AUTH_URL") ]]
 then
