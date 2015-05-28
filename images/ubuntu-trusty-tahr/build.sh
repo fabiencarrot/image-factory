9f7a86b34d4c183e8fc24e93cf2bd2f7d3f480ae
Maury Aurelien <aurelienmaury@users.noreply.github.com>
2015-05-28 17:01:15 +0200

    exec

--- images/ubuntu-trusty-tahr/build.sh
+++ images/ubuntu-trusty-tahr/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+LOG="./build.debug.log"
 BASENAME="ubuntu-14.04"
 TENANT_ID="772be1ffb32e42a28ac8e0205c0b0b90"
 BUILDMARK="$(date +%Y-%m-%d-%H%M)"
@@ -16,7 +17,7 @@ if [ -f "$IMG" ]; then
     rm $IMG
 fi
 
-wget $IMG_URL
+wget -q $IMG_URL
 
 if [ ! -d "$TMP_DIR" ]; then
     mkdir $TMP_DIR
@@ -51,24 +52,28 @@ TMP_IMG_ID="$(glance image-list --owner $TENANT_ID --is-public False | grep $TMP
 
 packer build -var "source_image=$TMP_IMG_ID" -var "image_name=$IMG_NAME" ../apt-bootstrap.packer.json
 
-glance image-delete $TMP_IMG_NAME
+echo "======= Deleting temporary image..."
+
+glance image-delete $TMP_IMG_NAME >> $LOG 2>&1
 
 IMG_ID="$(glance image-list --owner $TENANT_ID --is-public False | grep $IMG_NAME | tr "|" " " | tr -s " " | cut -d " " -f2)"
 
-glance image-update --property cw_os=Ubuntu --property cw_origin=Cloudwatt --property hw_rng_model=virtio --min-disk 10 --purge-props $IMG_ID
+glance image-update --property cw_os=Ubuntu --property cw_origin=Cloudwatt --property hw_rng_model=virtio --min-disk 10 --purge-props $IMG_ID  >> $LOG 2>&1
 
 FREE_FLOATING_IP="$(neutron floatingip-list | grep -v "+" | grep -v "id" | tr -d " " | grep -v -E "^\|.+\|.+\|.+\|.+\|$" | cut -d "|" -f 2)"
 
-echo "======= Cleaning unassociated floating ips"
+echo "======= Cleaning unassociated floating ips..."
 
 for floating_id in $FREE_FLOATING_IP; do
-    neutron floatingip-delete $floating_id
+    neutron floatingip-delete $floating_id >> $LOG 2>&1
 done
 
-echo "======= Cleaning too old images"
+echo "======= Cleaning old images..."
 
-glance image-list | grep $BASENAME | tr "|" " " | tr -s " " |cut -d " " -f 3 | sort -r | awk 'NR>5' | xargs glance image-delete
+glance image-list | grep $BASENAME | tr "|" " " | tr -s " " |cut -d " " -f 3 | sort -r | awk 'NR>5' | xargs glance image-delete >> $LOG 2>&1
 
-glance image-show $IMG_ID
+glance image-show $IMG_ID >> $LOG 2>&1
+
+echo "======= Validation testing..."
 
 URCHIN_IMG_ID=$IMG_ID $WORKSPACE/test-tools/urchin $WORKSPACE/test-tools/ubuntu-tests
