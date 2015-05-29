f512c9fdb3858348b5297df756398605311138f7
Maury Aurelien <aurelienmaury@users.noreply.github.com>
2015-05-29 11:40:10 +0200

    packer debug

--- factory-env.sh
+++ factory-env.sh
@@ -7,7 +7,7 @@ export BASE_IMG_UBUNTU_TRUSTY="d1f0726c-1186-4269-948e-72f6493d93c2"
 
 # Network ID to use for VM deployment during build
 export FACTORY_NETWORK="17decd89-56a2-4729-8bd6-453ebaa51860"
-export FACTORY_SECURITY_GROUP="FACTORY-SG-ZH6NLTYBM7FY"
+export FACTORY_SECURITY_GROUP="FACTORY-sg-zh6nltybm7fy"
 
 # Floating IP pool to use
 export FACTORY_FLOATING_IP_POOL="6ea98324-0f14-49f6-97c0-885d1b8dc517"
