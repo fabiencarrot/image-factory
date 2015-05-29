c8978bd78c77967b2ec5bdcfdc5304353c793613
Maury Aurelien <aurelienmaury@users.noreply.github.com>
2015-05-29 11:21:50 +0200

    cleanup starting

--- images/bundle-trusty-lamp/build.sh
+++ images/bundle-trusty-lamp/build.sh
@@ -1,38 +1,63 @@
 #!/bin/sh
 
 BASENAME="bundle-trusty-lamp"
-TENANT_ID="772be1ffb32e42a28ac8e0205c0b0b90"
+CW_BUNDLE="LAMP"
+
+if [ ! "$OS_TENANT_ID" ]; then
+    echo "OS_TENANT_ID env variable is mandatory"
+    exit 1
+fi
+
+SELF_PATH=`dirname "$0"`
+FACTORY_ENV="$SELF_PATH/../../factory-env.sh"
+
+. $FACTORY_ENV
+
+if [ "$?" -ne "0" ]; then
+    echo "Could not source factory environment: $FACTORY_ENV"
+    exit 1
+fi
+
+PACKER_FILE="../bundle-bootstrap.packer.json"
 BUILDMARK="$(date +%Y-%m-%d-%H%M)"
 IMG_NAME="$BASENAME-$BUILDMARK"
 TMP_IMG_NAME="$IMG_NAME-tmp"
-CW_BUNDLE="LAMP"
+SRC_IMG="$BASE_IMG_UBUNTU_TRUSTY"
 
+echo "======= Packer provisionning..."
+packer build -var "source_image=$SRC_IMG" -var "image_name=$IMG_NAME" $PACKER_FILE
 
-# Ubuntu Trusty
-SRC_IMG="f6f5a4cf-034b-443b-9d50-3865f76c4ac0"
+echo "======= Glance upload..."
+IMG_ID="$(glance image-list --owner $OS_TENANT_ID --is-public False | grep $IMG_NAME | tr "|" " " | tr -s " " | cut -d " " -f2)"
+glance image-update \
+    --property cw_bundle=$CW_BUNDLE \
+    --property cw_os=Ubuntu \
+    --property cw_origin=Cloudwatt \
+    --property hw_rng_model=virtio \
+    --min-disk 10 \
+    --purge-props $IMG_ID
 
-packer build -var "source_image=$SRC_IMG" -var "image_name=$IMG_NAME" ./lamp-bootstrap.packer.json
-
-IMG_ID="$(glance image-list --owner $TENANT_ID --is-public False | grep $IMG_NAME | tr "|" " " | tr -s " " | cut -d " " -f2)"
-
-glance image-update --property cw_bundle=$CW_BUNDLE --property cw_os=Ubuntu --property cw_origin=Cloudwatt --property hw_rng_model=virtio --min-disk 10 --purge-props $IMG_ID
-
-FREE_FLOATING_IP="$(neutron floatingip-list | grep -v "+" | grep -v "id" | tr -d " " | grep -v -E "^\|.+\|.+\|.+\|.+\|$" | cut -d "|" -f 2)"
 
 echo "======= Cleaning unassociated floating ips"
+
+FREE_FLOATING_IP="$(neutron floatingip-list | grep -v "+" | grep -v "id" | tr -d " " | grep -v -E "^\|.+\|.+\|.+\|.+\|$" | cut -d "|" -f 2)"
 
 for floating_id in $FREE_FLOATING_IP; do
     neutron floatingip-delete $floating_id
 done
 
-#echo "======= Cleaning too old images"
+echo "======= Cleaning too old images"
 
-#glance image-list | grep $BASENAME | tr "|" " " | tr -s " " | cut -d " " -f 3 | sort -r | awk 'NR>5' | xargs -r glance image-delete
+glance image-list | grep $BASENAME | tr "|" " " | tr -s " " | cut -d " " -f 3 | sort -r | awk 'NR>5' | xargs -r glance image-delete
+
+
+echo "======= Generating Heat template"
+
+if [ ! -d "$SELF_PATH/target" ]; then
+    mkdir $SELF_PATH/target
+fi
+sed "s/\\\$IMAGE\\\$/$IMG_ID/g" $SELF_PATH/heat/bundle-trusty-lamp.heat.yml > $SELF_PATH/target/bundle-trusty-lamp.heat.yml
+
+echo "======= Image detail"
 
 glance image-show $IMG_ID
-
-if [ ! -d "target" ]; then
-    mkdir target
-fi
-
-sed "s/\\\$IMAGE\\\$/$IMG_ID/g" heat/bundle-trusty-lamp.heat.yml > target/bundle-trusty-lamp.heat.yml
