b84e114daed5195023188af1f8315cf5718b04fb
Maury Aurelien <aurelienmaury@users.noreply.github.com>
2015-05-29 11:51:29 +0200

    packer debug

--- factory-env.sh
+++ factory-env.sh
@@ -10,4 +10,6 @@ export FACTORY_NETWORK="17decd89-56a2-4729-8bd6-453ebaa51860"
 export FACTORY_SECURITY_GROUP="FACTORY-sg-zh6nltybm7fy"
 
 # Floating IP pool to use
+# packer openstack builder does not interpolate var for ip_pool
+# so this value should also be put as-is in place in your packer files.
 export FACTORY_FLOATING_IP_POOL="6ea98324-0f14-49f6-97c0-885d1b8dc517"
