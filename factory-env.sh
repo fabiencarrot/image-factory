045765895012ea48743e50d7c5ee045e263ae4b3
Maury Aurelien <aurelienmaury@users.noreply.github.com>
2015-07-02 11:07:07 +0200

    my baaad

--- factory-env.sh
+++ factory-env.sh
@@ -3,7 +3,7 @@
 # This file should be source to make the scripts take all values specific to a factory instance.
 
 # ID of validated base system images
-export BASE_IMG_UBUNTU_TRUSTY="d1f0726c-1186-4269-948e-72f6493d93c2"
+export BASE_IMG_UBUNTU_TRUSTY="ae3082cb-fac1-46b1-97aa-507aaa8f184f"
 
 # Network ID to use for VM deployment during build
 export FACTORY_NETWORK="17decd89-56a2-4729-8bd6-453ebaa51860"
